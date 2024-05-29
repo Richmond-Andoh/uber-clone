023184eecab2810af32a3d07421c13ce623b8a35
richmond-andoh <richmondandoh00@gmail.com>
2024-05-29 08:40:57 +0000

    Home screen added

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to the Home Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
