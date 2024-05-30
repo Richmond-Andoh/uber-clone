a1ef582b0bd40b0be08e35b549b12e6ce4b037b5
richmond-andoh <richmondandoh00@gmail.com>
2024-05-30 05:39:07 +0000

    Modify NavOptions component

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,5 +1,6 @@
 import { FlatList, Text, TouchableOpacity, View, Image } from 'react-native'
 import tw from "twrnc";
+import { Icon } from 'react-native-elements';
 
 const data = [
     {
@@ -23,7 +24,7 @@ const NavOptions = () => {
       data={data}
       keyExtractor={(item) => item.id}
       renderItem={({item}) => (
-        <TouchableOpacity style={[tw`pl-8 pt-5 pb-4 pr-4 m-2 bg-gray-200`]}>
+        <TouchableOpacity style={[tw`pl-4 pt-5 pb-4 pr-4 m-2 bg-gray-200`]}>
             <View>
             <Image 
                 style={{width: 120, height: 120, resizeMode: "contain"}}
@@ -31,7 +32,12 @@ const NavOptions = () => {
                     uri: item.img,
                 }}
             />
-                <Text>{item.title}</Text>
+                <Text style={[tw`text-center pt-4`]}>{item.title}</Text>
+                <Icon
+                  name='arrowright'
+                  style={[tw`bg-primary-content`]}
+                  type="antdesign"
+                />
             </View>
         </TouchableOpacity>
       )}
