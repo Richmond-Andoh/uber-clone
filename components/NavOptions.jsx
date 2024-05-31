674b0f359c34c3f790e89bce5e157c6c36f70383
richmond-andoh <richmondandoh00@gmail.com>
2024-05-31 13:49:18 +0000

    Some fixes in the screen rrendering

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,23 +1,23 @@
-import { FlatList, Text, TouchableOpacity, View, Image } from 'react-native'
+import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
 import tw from "twrnc";
-import { Icon } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
-    {
-        id: "1",
-        title: "Get a Ride",
-        img: "https://links.papareact.com/3pn",
-        screen: "MapScreen"
-    },
+  {
+    id: "1",
+    title: "Get a Ride",
+    img: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
 
-    {
-        id: "123",
-        title: "Order Food",
-        img: "https://links.papareact.com/28w",
-        screen: "EatScreen"
-    }
-]
+  {
+    id: "123",
+    title: "Order Food",
+    img: "https://links.papareact.com/28w",
+    screen: "EatScreen",
+  },
+];
 const NavOptions = () => {
   const navigation = useNavigation();
   return (
@@ -25,30 +25,30 @@ const NavOptions = () => {
       horizontal
       data={data}
       keyExtractor={(item) => item.id}
-      renderItem={({item}) => (
-        <TouchableOpacity 
-          onPress={() => navigation.navigate("MapScreen")}
-          style={[tw`pl-4 pt-5 pb-4 pr-4 m-2 bg-gray-200 rounded-lg`]}>
-            <View>
-            <Image 
-                style={{width: 120, height: 120, resizeMode: "contain"}}
-                source={{
-                    uri: item.img,
-                }}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          style={[tw`pl-4 pt-5 pb-4 pr-4 m-2 bg-gray-200 rounded-lg`]}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{
+                uri: item.img,
+              }}
             />
-                <Text style={[tw`pt-4`]}>{item.title}</Text>
-                <Icon
-                  name='arrowright'
-                  style={[tw`bg-black rounded-full w-10 p-2 mt-4`]}
-                  type="antdesign"
-                  color="white"
-                />
-            </View>
+            <Text style={[tw`pt-4`]}>{item.title}</Text>
+            <Icon
+              onPress={() => navigation.navigate("MapScreen")}
+              name="arrowright"
+              style={[tw`bg-black rounded-full w-10 p-2 mt-4`]}
+              type="antdesign"
+              color="white"
+            />
+          </View>
         </TouchableOpacity>
       )}
     />
-  )
-}
+  );
+};
 
-export default NavOptions
-
+export default NavOptions;
--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,5 +1,4 @@
 import { Text, View } from 'react-native'
-import React from 'react'
 
 const MapScreen = () => {
   return (
@@ -9,5 +8,5 @@ const MapScreen = () => {
   )
 }
 
-export default MapScreen
+export default MapScreen;
 
